6f624ca90d0430e372853dffe9af36083b764caa
victoryckl <victoryckl2012@gmail.com>
2014-01-08 00:30:50 +0800

    添加布局protect_window_layout，目前在WebView中，视频和swf都无法播放 #	modified:   ProtectEye/ProtectEyeService/AndroidManifest.xml #	new file:   ProtectEye/ProtectEyeService/assets/g.html #	new file:   ProtectEye/ProtectEyeService/assets/g.swf #	new file:   ProtectEye/ProtectEyeService/assets/g2.html #	new file:   ProtectEye/ProtectEyeService/assets/g2.swf #	new file:   ProtectEye/ProtectEyeService/assets/v.html #	new file:   ProtectEye/ProtectEyeService/assets/v.mp4 #	new file:   ProtectEye/ProtectEyeService/res/layout/protect_window_layout.xml #	modified:   ProtectEye/ProtectEyeService/src/com/example/protecteye/service/ProtectWindowService.java

--- ProtectEye/ProtectEyeService/src/com/example/protecteye/service/ProtectWindowService.java
+++ ProtectEye/ProtectEyeService/src/com/example/protecteye/service/ProtectWindowService.java
@@ -1,5 +1,6 @@
 package com.example.protecteye.service;
 
+import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -7,19 +8,24 @@ import android.graphics.PixelFormat;
 import android.os.IBinder;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebSettings.PluginState;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.utils.Common;
 
+@SuppressLint("NewApi")
 public class ProtectWindowService extends Service {
 
 	private boolean isAdded = false; // 是否已增加悬浮窗
 	private static WindowManager wm;
 	private static WindowManager.LayoutParams params;
-	private Button btn_floatView;
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -56,25 +62,36 @@ public class ProtectWindowService extends Service {
 
 	private void showWindow() {
 		if (!isAdded) {
-			wm.addView(btn_floatView, params);
+			wm.addView(mWindow, params);
 			isAdded = true;
 		}
 	}
 	
 	private void hideWindow() {
 		if (isAdded) {
-			wm.removeView(btn_floatView);
+			wm.removeView(mWindow);
 			isAdded = false;
 		}
 	}
 
+	private ViewGroup mWindow;
 	/**
 	 * 创建悬浮窗
 	 */
 	private void createFloatView() {
-		btn_floatView = new Button(getApplicationContext());
-		btn_floatView.setText("$_$");
-		btn_floatView.setBackgroundColor(getResources().getColor(android.R.color.darker_gray));
+		mWindow = (ViewGroup) View.inflate(getApplicationContext(),
+				R.layout.protect_window_layout, null);
+		
+		WebView webView = (WebView) mWindow.findViewById(R.id.wv_webview);
+		webSettings(webView);
+		
+		Button btnClose = (Button) mWindow.findViewById(R.id.btn_close);
+		btnClose.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				hideWindow();
+			}
+		});
 
 		wm = (WindowManager) getApplicationContext().getSystemService(
 				Context.WINDOW_SERVICE);
@@ -99,39 +116,21 @@ public class ProtectWindowService extends Service {
 		 */
 
 		// 设置悬浮窗的长得宽
-		params.width = getResources().getDimensionPixelSize(R.dimen.float_width);
-		params.height = getResources().getDimensionPixelSize(R.dimen.float_height);
+		params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+		params.height = ViewGroup.LayoutParams.MATCH_PARENT;
 
-		// 设置悬浮窗的Touch监听
-		btn_floatView.setOnTouchListener(new OnTouchListener() {
-			int lastX, lastY;
-			int paramX, paramY;
-
-			public boolean onTouch(View v, MotionEvent event) {
-				switch (event.getAction()) {
-				case MotionEvent.ACTION_DOWN:
-					lastX = (int) event.getRawX();
-					lastY = (int) event.getRawY();
-					paramX = params.x;
-					paramY = params.y;
-					break;
-				case MotionEvent.ACTION_MOVE:
-					int dx = (int) event.getRawX() - lastX;
-					int dy = (int) event.getRawY() - lastY;
-					params.x = paramX + dx;
-					params.y = paramY + dy;
-					// 更新悬浮窗位置
-					wm.updateViewLayout(btn_floatView, params);
-					break;
-				case MotionEvent.ACTION_UP:
-					Toast.makeText(getApplicationContext(), "touch up !!", Toast.LENGTH_SHORT).show();
-					break;
-				}
-				return true;
-			}
-		});
-		
-		wm.addView(btn_floatView, params);
+		wm.addView(mWindow, params);
 		isAdded = true;
+		webView.loadUrl("file:///android_asset/v.html");
+	}
+	
+	private void webSettings(WebView view) {
+		WebSettings s = view.getSettings();
+		
+		s.setJavaScriptEnabled(true);
+		s.setPluginState(PluginState.ON);
+		s.setTextZoom(100);
+		
+		view.setBackgroundColor(0x88ffff00);
 	}
 }
