f9fd2c2bcaa1ae5b41d9e19d599c59af19045d8a
chenkongliang <victoryckl@tpxip.com>
2014-05-28 11:27:30 +0800

    增加PackageUtils.java #	modified:   DisableReceiverInCode/res/layout/main.xml #	modified:   DisableReceiverInCode/src/com/cybertron/account/util/PackageUtils.java #	modified:   DisableReceiverInCode/src/com/rsft/test/demo1/DisableReceiverInCodeActivity.java #

--- DisableReceiverInCode/src/com/cybertron/account/util/PackageUtils.java
+++ DisableReceiverInCode/src/com/cybertron/account/util/PackageUtils.java
@@ -2,12 +2,13 @@ package com.cybertron.account.util;
 
 import java.util.List;
 
+import android.R.anim;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.ResolveInfo;
 import android.util.Log;
 
 public class PackageUtils {
@@ -26,24 +27,55 @@ public class PackageUtils {
 		return pm.getInstalledApplications(flags);
 	}
 	
-	public static void printAppInfos(List<ApplicationInfo> infos) {
+	public static void printAppInfos(PackageManager pm, List<ApplicationInfo> infos) {
 		for (int i=0; i<infos.size(); i++) {
 			ApplicationInfo info = infos.get(i);
-			boolean isBoot = ((info.flags & PackageManager.COMPONENT_ENABLED_STATE_DEFAULT) != 0 
-					|| (info.flags & PackageManager.COMPONENT_ENABLED_STATE_ENABLED) != 0);
-			Log.i(TAG, info.packageName + ": " + isBoot);
+			int state = pm.getApplicationEnabledSetting(info.packageName);
+			boolean isEnable = toEnable(state);
+			Log.i(TAG, "" + isEnable + ", " + info.loadLabel(pm) + ", "
+					+ info.packageName);
 		}
 	}
 	
-    public static List<ResolveInfo> listBootStartReceivers(PackageManager pm) {
-    	Intent intent = new Intent(Intent.ACTION_BOOT_COMPLETED);
+    public static List<ResolveInfo> listBootReceivers(PackageManager pm) {
+    	return listReceivers(pm, Intent.ACTION_BOOT_COMPLETED);
+    }
+    
+    public static List<ResolveInfo> listReceivers(PackageManager pm, String action) {
+    	Intent intent = new Intent(action);
 		return pm.queryBroadcastReceivers(intent,
 				PackageManager.GET_DISABLED_COMPONENTS);
     }
-	
+    
+    public static void printBootStartReceivers(PackageManager pm) {
+    	List<ResolveInfo> list = listBootReceivers(pm);
+    	for (int i=0; i<list.size(); i++) {
+    		ResolveInfo ri = list.get(i);
+			ComponentName componentName = new ComponentName(
+					ri.activityInfo.packageName, ri.activityInfo.name);
+    		int state = pm.getComponentEnabledSetting(componentName);
+    		boolean isBoot = isComponentEnable(pm, componentName);
+			Log.i(TAG, "" + state + "," + isBoot + "," + ri.loadLabel(pm) + ","
+					+ componentName.flattenToString());
+    	}
+    }
+    
+    public static boolean isAppEnable(PackageManager pm, String packageName) {
+		int state = pm.getApplicationEnabledSetting(packageName);
+		return toEnable(state);
+    }
+    
+    public static void setAppEnable(PackageManager pm, String packageName, boolean enable) {
+    	if (enable != isAppEnable(pm, packageName)) {
+    		int newState = toState(enable);
+			pm.setApplicationEnabledSetting(packageName, newState,
+					PackageManager.DONT_KILL_APP);
+    	}
+    }
+    
 	public static boolean isSystemApp(ApplicationInfo info) {
 		if ((info.flags & ApplicationInfo.FLAG_SYSTEM) != 0
-				|| (info.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
+			|| (info.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
 			return true;
 		}
 		return false;
@@ -55,16 +87,40 @@ public class PackageUtils {
 		return isSystemApp(info);
 	}
 
-	public static boolean isBootStart(PackageManager pm,
+	public static boolean isComponentEnable(PackageManager pm,
 			ComponentName componentName) {
-		int flag = pm.getComponentEnabledSetting(componentName);
-		return ((flag == PackageManager.COMPONENT_ENABLED_STATE_DEFAULT) 
-				|| (flag == PackageManager.COMPONENT_ENABLED_STATE_ENABLED));
+		int state = pm.getComponentEnabledSetting(componentName);
+		return toEnable(state);
 	}
 
-	public static boolean isBootStart(PackageManager pm, String packageName,
+	public static boolean isComponentEnable(PackageManager pm, String packageName,
 			String cls) {
 		ComponentName componentName = new ComponentName(packageName, cls);
-		return isBootStart(pm, componentName);
+		return isComponentEnable(pm, componentName);
+	}
+	
+	public static void setComponentEnable(PackageManager pm,
+			ComponentName componentName, boolean enable) {
+		if (isComponentEnable(pm, componentName) != enable) {
+			int newState = toState(enable);
+			pm.setComponentEnabledSetting(componentName, newState,
+					PackageManager.DONT_KILL_APP);
+		}
+	}
+
+	public static void setComponentEnable(PackageManager pm, String packageName,
+			String cls, boolean enable) {
+		ComponentName componentName = new ComponentName(packageName, cls);
+		setComponentEnable(pm, componentName, enable);
+	}
+	
+	public static int toState(boolean enable) {
+		return enable ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED 
+	                  : PackageManager.COMPONENT_ENABLED_STATE_DISABLED;
+	}
+	
+	public static boolean toEnable(int state) {
+		return ((state == PackageManager.COMPONENT_ENABLED_STATE_DEFAULT) 
+		     || (state == PackageManager.COMPONENT_ENABLED_STATE_ENABLED));
 	}
 }
--- DisableReceiverInCode/src/com/rsft/test/demo1/DisableReceiverInCodeActivity.java
+++ DisableReceiverInCode/src/com/rsft/test/demo1/DisableReceiverInCodeActivity.java
@@ -12,17 +12,22 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
 
 public class DisableReceiverInCodeActivity extends Activity {
+	private PackageManager pm;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        pm = getPackageManager();
 
         final CheckBox bootCheck = (CheckBox) findViewById(R.id.boot_complete_checkbox);
         final ComponentName cm = new ComponentName("com.rsft.test.demo1", "com.rsft.test.demo1.MyReceiver");
-        final PackageManager pm = getPackageManager();
         int state = pm.getComponentEnabledSetting(cm);
         if (state != PackageManager.COMPONENT_ENABLED_STATE_DISABLED
                 && state != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER) {
@@ -39,19 +44,73 @@ public class DisableReceiverInCodeActivity extends Activity {
         });
         
         listBootStartApp();
+        
+        initViews();
     }
     
-    private void listBootStartApp() {
+	private void listBootStartApp() {
     	new Thread() {
     		@Override
     		public void run() {
-    			List<ApplicationInfo> infos = PackageUtils.listApps(getPackageManager());
-    			PackageUtils.printAppInfos(infos);
-    			
-    	        BootStartUtils utils = new BootStartUtils(getApplicationContext());
-    	        utils.fetchInstalledApps();
-    	        utils.printBootStartReceivers();
+    			List<ApplicationInfo> infos = PackageUtils.listApps(getPackageManager(), 0);
+    			PackageUtils.printAppInfos(pm, infos);
+    			PackageUtils.printBootStartReceivers(pm);
     		}
     	}.start();
     }
+	
+	private CheckBox mCbApp, mCbComp;
+	private EditText mEtAppPkg, mEtCompPkg, mEtCompCls;
+	private void initViews() {
+		mEtAppPkg = (EditText) findViewById(R.id.et_app_pacakge);
+		mEtCompPkg = (EditText) findViewById(R.id.et_comp_package);
+		mEtCompCls = (EditText) findViewById(R.id.et_comp_cls);
+		
+		mCbApp  = (CheckBox) findViewById(R.id.cp_enable_app);
+		mCbComp = (CheckBox) findViewById(R.id.cp_enable_component);
+		
+		mCbApp.setOnCheckedChangeListener(mChangeListener);
+		mCbComp.setOnCheckedChangeListener(mChangeListener);
+		
+		mCbApp.setChecked(isAppEnable());
+		mCbComp.setChecked(isCompEnable());
+	}
+	
+	private OnCheckedChangeListener mChangeListener = new OnCheckedChangeListener() {
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+			switch (buttonView.getId()) {
+			case R.id.cp_enable_app:
+				setAppEnable(isChecked);
+				break;
+			case R.id.cp_enable_component:
+				setCompEnable(isChecked);
+				break;
+			default:
+				break;
+			}
+		}
+	};
+	
+	private void setAppEnable(boolean isChecked) {
+		String pkg = mEtAppPkg.getText().toString();
+		PackageUtils.setAppEnable(pm, pkg, isChecked);
+	}
+	
+	private void setCompEnable(boolean isChecked) {
+		String pkg = mEtCompPkg.getText().toString();
+		String cls = mEtCompCls.getText().toString();
+		PackageUtils.setComponentEnable(pm, pkg, cls, isChecked);
+	}
+	
+	private boolean isAppEnable() {
+		String pkg = mEtAppPkg.getText().toString();
+		return PackageUtils.isAppEnable(pm, pkg);
+	}
+	
+	private boolean isCompEnable() {
+		String pkg = mEtCompPkg.getText().toString();
+		String cls = mEtCompCls.getText().toString();
+		return PackageUtils.isComponentEnable(pm, pkg, cls);
+	}
 }
