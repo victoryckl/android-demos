64a5c2dc175be80f2a1de49a06677c7e33d73ada
chenkongliang <victoryckl@tpxip.com>
2014-06-11 15:05:20 +0800

    AirAppEntryDemo可以正常播放swf了 #	modified:   AirAppEntryDemo/AndroidManifest.xml #	modified:   AirAppEntryDemo/assets/app.swf #	deleted:    AirAppEntryDemo/jar/AppEntry_debug.jar #	renamed:    AirAppEntryDemo/jar/AppEntry_release.jar -> AirAppEntryDemo/libs/AppEntry_release.jar #	new file:   AirAppEntryDemo/res/drawable-hdpi/icon.png #	modified:   AirAppEntryDemo/res/drawable-ldpi/icon.png #	modified:   AirAppEntryDemo/res/drawable-mdpi/icon.png #	modified:   AirAppEntryDemo/res/drawable/mp_warning_32x32_n.png #	modified:   AirAppEntryDemo/res/layout/activity_main.xml #	new file:   AirAppEntryDemo/res/raw/rgba8888 #	new file:   AirAppEntryDemo/res/values/drawables.xml #	new file:   AirAppEntryDemo/res/values/public.xml #	new file:   AirAppEntryDemo/res/values/raws.xml #	modified:   AirAppEntryDemo/res/values/strings.xml #	deleted:    AirAppEntryDemo/src/com/adobe/air/ResourceIdMap.java #	deleted:    AirAppEntryDemo/src/com/adobe/air/appentry/AppEntry.java #	deleted:    AirAppEntryDemo/src/com/adobe/air/appentry/GetVersionCode.java #	modified:   AirAppEntryDemo/src/com/example/airappentrydemo/MainActivity.java #

--- AirAppEntryDemo/src/com/adobe/air/appentry/GetVersionCode.java
+++
@@ -1,7 +0,0 @@
-package com.adobe.air.appentry;
-
-public class GetVersionCode {
-	public static void main(String[] args) {
-		System.out.println("0.0.0");
-	}
-}
--- AirAppEntryDemo/src/com/example/airappentrydemo/MainActivity.java
+++ AirAppEntryDemo/src/com/example/airappentrydemo/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.airappentrydemo;
 
+import air.com.adobe.appentry.AppEntry;
 import android.os.Bundle;
-
-import com.adobe.air.appentry.AppEntry;
+import android.os.Process;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppEntry {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-//		setContentView(R.layout.activity_main);
+		
+		// Ä¬ÈÏÈí¼üÅÌ²»µ¯³ö 
+		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+		
+		FrameLayout layout = (FrameLayout) findViewById(android.R.id.content);
+		LinearLayout linearLayout = (LinearLayout) View.inflate(this, R.layout.activity_main, null);
+		layout.addView(linearLayout);
+		
+		findViewById(R.id.btn_close).setOnClickListener(mClickListener);
 	}
+	
+	private OnClickListener mClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.btn_close:
+				MainActivity.this.finish();
+				Process.killProcess(Process.myPid());
+				break;
+			default:
+				break;
+			}
+		}
+	};
 }
