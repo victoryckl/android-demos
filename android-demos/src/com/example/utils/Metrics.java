9cad330a63d0aaed0512bc353e230c348cfc95a4
chenkongliang <victoryckl@tpxip.com>
2014-06-20 15:04:45 +0800

    add Metrics.getStatusBarHeight() #	modified:   android-demos/src/com/example/utils/Metrics.java #

--- android-demos/src/com/example/utils/Metrics.java
+++ android-demos/src/com/example/utils/Metrics.java
@@ -1,28 +1,36 @@
 package com.example.utils;
 
-import android.app.Activity;
+import java.lang.reflect.Field;
+
+import android.content.Context;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
 
 public class Metrics {
 	private static final String TAG = Metrics.class.getSimpleName();
 	private static DisplayMetrics mMetrics = new DisplayMetrics();
+	private static int mStatusBarHeight;
 	
 	private Metrics() {}
 	
-	public static void init(Activity activity) {
-		activity.getWindowManager().getDefaultDisplay().getMetrics(mMetrics);
+	public static void initialize(Context context) {
+		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		wm.getDefaultDisplay().getMetrics(mMetrics);
+		mStatusBarHeight = initStatusBarHeight(context);
 		Log.i(TAG, "density: " + mMetrics.density
 				+ "\ndensityDpi: " + mMetrics.densityDpi
 				+ "\nscaledDensity: " + mMetrics.scaledDensity
 				+ "\nwidthPixels: "  + mMetrics.widthPixels
 				+ "\nheightPixels: " + mMetrics.heightPixels
 				+ "\nxdpi: " + mMetrics.xdpi
-				+ "\nydpi: " + mMetrics.xdpi);
+				+ "\nydpi: " + mMetrics.xdpi
+				+ "\nmStatusBarHeight: " + mStatusBarHeight);
 	}
 	
 	private static void check() {
 		if (mMetrics.density < 0.01f) {
-			throw new RuntimeException("please call Metrics.init() first!!");
+			throw new RuntimeException("please call Metrics.initialize() first!!");
 		}
 	}
 	
@@ -40,4 +48,42 @@ public class Metrics {
 		check();
 		return dp*mMetrics.density;
 	}
+	
+	public static DisplayMetrics getDisplayMetrics() {
+		check();
+		return mMetrics;
+	}
+	
+	public static int getWidth() {
+		check();
+		return mMetrics.widthPixels;
+	}
+	
+	public static int getHeight() {
+		check();
+		return mMetrics.heightPixels;
+	}
+	
+	//---------------
+	public static int getStatusBarHeight(){
+		check();
+		return mStatusBarHeight;
+	}
+	
+	private static int initStatusBarHeight(Context context){
+        Class<?> c = null;
+        Object obj = null;
+        Field field = null;
+        int x = 0, statusBarHeight = 0;
+        try {
+            c = Class.forName("com.android.internal.R$dimen");
+            obj = c.newInstance();
+            field = c.getField("status_bar_height");
+            x = Integer.parseInt(field.get(obj).toString());
+            statusBarHeight = context.getResources().getDimensionPixelSize(x);
+        } catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        return statusBarHeight;
+    }
 }
