a6fe46ac1337da3055ec9059411c1b5a4b6e71de
chenkongliang <victoryckl@tpxip.com>
2014-09-11 17:16:59 +0800

    AirAppEntryDemo： 1.application.xml中air请求版本由2.6改为3.8，解决播放部分swf时，闪退的问题 	modified:   AirAppEntryDemo/AirAppEntryDemo/assets/META-INF/AIR/application.xml 	renamed:    AirAppEntryDemo/AirAppEntryDemo/assets/app.swf -> AirAppEntryDemo/AirAppEntryDemo/assets/app-2.swf 	modified:   AirAppEntryDemo/AirAppEntryDemo/assets/app.swf 	modified:   AirAppEntryDemo/AirAppEntryDemo/src/com/example/airappentrydemo/MainActivity.java 	modified:   AirAppEntryDemo/AppEntryLibrary/.classpath 	renamed:    AirAppEntryDemo/AppEntryLibrary/libs/AppEntry_release.jar -> AirAppEntryDemo/AppEntryLibrary/libs/AppEntry_debug.jar 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-cs/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-de/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-es/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-fr/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-it/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-ja/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-ko/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-nl/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-pl/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-pt/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-ru/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-sv/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-tr/strings.xml 	modified:   AirAppEntryDemo/AppEntryLibrary/res/values-zh-rCN/strings.xml 	deleted:    AirAppEntryDemo/AppEntryLibrary/res/values-zh-rTW/strings.xml 	modified:   AirAppEntryDemo/AppEntryLibrary/res/values/strings.xml

--- AirAppEntryDemo/AirAppEntryDemo/src/com/example/airappentrydemo/MainActivity.java
+++ AirAppEntryDemo/AirAppEntryDemo/src/com/example/airappentrydemo/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.airappentrydemo;
 
+import java.io.File;
+
 import air.com.adobe.appentry.AppEntry;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.os.Process;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
@@ -10,9 +16,13 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppEntry {
-
+	private static final String TAG = MainActivity.class.getSimpleName();
+	private String mUnique, mContentName, mLinkPath;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		initMetaData();
+		
 		super.onCreate(savedInstanceState);
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
 		
@@ -41,5 +51,27 @@ public class MainActivity extends AppEntry {
 	public void finish() {
 		super.finish();
 		Process.killProcess(Process.myPid());
-	};
+	}
+	
+	private void initMetaData() {
+		try {
+			ActivityInfo info = getPackageManager().getActivityInfo(
+					getComponentName(), PackageManager.GET_META_DATA);
+			Bundle meta = info.metaData;
+			mUnique = meta.getString("uniqueappversionid");
+			mContentName = meta.getString("initialcontent");
+			
+			File file = new File(new File(getCacheDir(), 
+					"app" + File.separator + mUnique + File.separator + "assets"), 
+					mContentName);
+			mLinkPath = file.getAbsolutePath();
+			Log.i(TAG, mLinkPath);
+			File f = new File(mLinkPath);
+			if (f.exists()) {
+				f.delete();
+			}
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
