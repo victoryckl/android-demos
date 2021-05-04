95b0d8afdb4792cd7263ccab412e1d3f82c57f66
chenkongliang <chenkongliang@haochengji100.com>
2021-05-04 20:25:02 +0800

    组合 / 合并操作符 	deleted:    .idea/codeStyles/Project.xml 	deleted:    .idea/gradle.xml 	deleted:    .idea/misc.xml 	deleted:    .idea/runConfigurations.xml 	deleted:    .idea/vcs.xml 	modified:   app/src/main/java/com/example/rx/demo/MainActivity.java 	modified:   app/src/main/res/layout/activity_main.xml

--- RxAndroidDemo/app/src/main/java/com/example/rx/demo/MainActivity.java
+++ RxAndroidDemo/app/src/main/java/com/example/rx/demo/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.example.util.StackTrace;
 
@@ -20,8 +19,11 @@ import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.ObservableSource;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.BiConsumer;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
 
 // RxJava系列教程 https://www.jianshu.com/nb/14302692
 // https://www.jianshu.com/p/a406b94f3188 Android Rxjava：这是一篇 清晰 & 易懂的Rxjava 入门教程
@@ -42,8 +44,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button_rx1: testRx1(); break;
-            case R.id.button_rx2: testRx2(); break;
+            case R.id.button_create1: testCreate1(); break;
+            case R.id.button_create2: testCreate2(); break;
             case R.id.button_just: testJust(); break;
             case R.id.button_array: testFromArray(); break;
             case R.id.button_iterable: testFromIterable(); break;
@@ -57,10 +59,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_flatmap: testFlatMap(); break;
             case R.id.btn_concatmap: testConcatMap(); break;
             case R.id.btn_buffer: testBuffer(); break;
+            case R.id.btn_concat: testConcat(); break;
+            case R.id.btn_concatArray: testConcatArray(); break;
+            case R.id.btn_merge: testMerge(); break;
+            case R.id.btn_mergeArray: testMergeArray(); break;
+            case R.id.btn_concat_error: testConcatError(); break;
+            case R.id.btn_zip: testZip(); break;
+            case R.id.btn_combine_latest: testCombineLatest(); break;
+            case R.id.btn_reduce: testReduce(); break;
+            case R.id.btn_collect: testCollect(); break;
+            case R.id.btn_start_with: testStartWith(); break;
+            case R.id.btn_count: testCount(); break;
         }
     }
 
-    private void testRx1() {
+    private void testCreate1() {
         //  1. 创建被观察者 Observable 对象
         Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
             // 2. 在复写的subscribe（）里定义需要发送的事件
@@ -78,13 +91,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
         //步骤2：创建观察者 Observer 并 定义响应事件行为
-        Observer<Integer> observer = new LogObserver<>("testRx1");
+        Observer<Integer> observer = new LogObserver<>("testCreate1");
 
         // 步骤3：通过订阅（subscribe）连接观察者和被观察者
         observable.subscribe(observer);
     }
 
-    private void testRx2() {
+    private void testCreate2() {
         // RxJava的流式操作
         Observable.create(new ObservableOnSubscribe<Integer>() {
             // 1. 创建被观察者 & 生产事件
@@ -98,7 +111,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         })
         // 2. 通过通过订阅（subscribe）连接观察者和被观察者
         // 3. 创建观察者 & 定义响应事件的行为
-        .subscribe(new LogObserver<Integer>("testRx2"));
+        .subscribe(new LogObserver<Integer>("testCreate2"));
     }
 
     private void testJust() {
@@ -324,6 +337,267 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     @Override
                     public void onComplete() {
                         Log.d(TAG, "[testBuffer] 对Complete事件作出响应");
+                    }
+                });
+    }
+
+    private void testConcat() {
+        // 组合多个被观察者一起发送数据，合并后 按发送顺序串行执行
+        // 被观察者数量≤4个
+        Observable.concat(
+                Observable.just(1,2),
+                Observable.fromArray(new Integer[]{3,4}),
+                Observable.range(5,3))
+        .subscribe(new LogObserver("testConcat"));
+    }
+
+    private void testConcatArray() {
+        // 组合多个被观察者一起发送数据，合并后 按发送顺序串行执行
+        // 被观察者数量可>4个
+        Observable.concatArray(
+                Observable.just(1,2),
+                Observable.fromArray(new Integer[]{3,4}),
+                Observable.range(5,3),
+                Observable.just(8,9),
+                Observable.just(10,11),
+                Observable.just(12,13))
+                .subscribe(new LogObserver("testConcatArray"));
+    }
+
+    private void testMerge() {
+        // 组合多个被观察者一起发送数据，合并后 按时间线并行执行
+        // 被观察者数量≤4个
+        Observable.merge(
+                //// 从0开始发送、共发送3个数据、第1次事件延迟发送时间 = 1s、间隔时间 = 1s
+                Observable.intervalRange(0, 3, 1, 1, TimeUnit.SECONDS),
+                // 从3开始发送、共发送3个数据、第1次事件延迟发送时间 = 1s、间隔时间 = 1s
+                Observable.intervalRange(3, 3, 1, 1, TimeUnit.SECONDS)
+        ).subscribe(new LogObserver("testMerge"));
+    }
+
+    private void testMergeArray() {
+        // 组合多个被观察者一起发送数据，合并后 按时间线并行执行
+        // 被观察者数量可>4个
+        Observable.merge(
+                //// 从0开始发送、共发送3个数据、第1次事件延迟发送时间 = 1s、间隔时间 = 1s
+                Observable.intervalRange(0, 3, 1, 1, TimeUnit.SECONDS),
+                // 从3开始发送、共发送3个数据、第1次事件延迟发送时间 = 1s、间隔时间 = 1s
+                Observable.intervalRange(3, 3, 1, 1, TimeUnit.SECONDS)
+        ).subscribe(new LogObserver("testMergeArray"));
+    }
+
+    private void testConcatError() {
+        //未使用concatDelayError（）的情况
+        Observable.concat(
+                Observable.create(new ObservableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                        e.onNext(1);
+                        e.onNext(2);
+                        e.onNext(3);
+                        // 发送Error事件，因为无使用concatDelayError，所以第2个Observable将不会发送事件
+                        e.onError(new NullPointerException());
+                        e.onComplete();
+                    }
+                }),
+                Observable.just(4,5,6))
+                .subscribe(new LogObserver<Integer>("testConcatError"));
+        //第1个被观察者发送Error事件后，第2个被观察者则不会继续发送事件
+
+
+        /////////////////////////////////////
+        //使用了 concatDelayError（）的情况
+        Observable.concatArrayDelayError(
+                Observable.create(new ObservableOnSubscribe<Integer>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                        e.onNext(1);
+                        e.onNext(2);
+                        e.onNext(3);
+                        // 发送Error事件，因为无使用concatDelayError，所以第2个Observable将不会发送事件
+                        e.onError(new NullPointerException());
+                        e.onComplete();
+                    }
+                }),
+                Observable.just(4,5,6))
+                .subscribe(new LogObserver<Integer>("testConcatError"));
+        //第1个被观察者的Error事件将在第2个被观察者发送完事件后再继续发送
+    }
+
+    private void testZip() {
+        //合并 多个被观察者（Observable）发送的事件，生成一个新的事件序列（即组合过后的事件序列），并最终发送
+        //事件组合方式 = 严格按照原先事件序列 进行对位合并
+        //最终合并的事件数量 = 多个被观察者（Observable）中数量最少的数量
+
+        //创建第1个被观察者
+        Observable<Integer> observable1 = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> e) throws Exception {
+                Log.i(TAG, "被观察者1发送了事件1");
+                e.onNext(1);
+                //为了方便展示效果，所以在发送事件后加入1s的延迟
+                Thread.sleep(1000);
+
+                Log.i(TAG, "被观察者1发送了事件2");
+                e.onNext(2);
+                Thread.sleep(1000);
+
+                Log.i(TAG, "被观察者1发送了事件3");
+                e.onNext(3);
+                Thread.sleep(1000);
+
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.io());// 设置被观察者1在工作线程1中工作
+
+        //创建第2个被观察者
+        Observable<String> observable2 = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                Log.i(TAG, "被观察者2发送了事件A");
+                e.onNext("A");
+                Thread.sleep(1000);
+
+                Log.i(TAG, "被观察者2发送了事件B");
+                e.onNext("B");
+                Thread.sleep(1000);
+
+                Log.i(TAG, "被观察者2发送了事件C");
+                e.onNext("C");
+                Thread.sleep(1000);
+
+                Log.i(TAG, "被观察者2发送了事件D");
+                e.onNext("D");
+                Thread.sleep(1000);
+
+                e.onComplete();
+            }
+        }).subscribeOn(Schedulers.newThread());// 设置被观察者2在工作线程2中工作
+        // 假设不作线程控制，则该两个被观察者会在同一个线程中工作，即发送事件存在先后顺序，而不是同时发送
+
+        //使用zip变换操作符进行事件合并
+        //注：创建BiFunction对象传入的第3个参数 = 合并后数据的数据类型
+        Observable.zip(observable1, observable2, new BiFunction<Integer, String, String>() {
+            @Override
+            public String apply(Integer integer, String s) throws Exception {
+                return integer+s;
+            }
+        }).subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.i(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(String value) {
+                Log.i(TAG, "最终接收到的事件 =  " + value);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete");
+            }
+        });
+    }
+
+    private void testCombineLatest() {
+        //当两个Observables中的任何一个发送了数据后，将先发送了数据的Observables 的最新（最后）一个数据 与 另外一个Observable发送的每个数据结合，最终基于该函数的结果发送数据
+        //与Zip（）的区别：Zip（） = 按个数合并，即1对1合并；CombineLatest（） = 按时间合并，即在同一个时间点上合并
+        Observable.combineLatest(
+                Observable.just("A", "B", "C"), //// 第1个发送数据事件的Observable
+                Observable.intervalRange(0, 3, 1, 1, TimeUnit.SECONDS),
+                new BiFunction<String, Long, String>() {
+                    @Override
+                    public String apply(String o1, Long o2) throws Exception {
+                        // o1 = 第1个Observable发送的最新（最后）1个数据
+                        // o2 = 第2个Observable发送的每1个数据
+                        Log.i(TAG, "合并的数据是： "+ o1 + "和"+ o2);
+                        return o1 + o2;
+                        //// 即第1个Observable发送的最后1个数据 与 第2个Observable发送的每1个数据进行合并
+                    }
+                }
+        ).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                Log.i(TAG, "合并的结果是： "+s);
+            }
+        });
+    }
+
+    private void testReduce() {
+        //把被观察者需要发送的事件聚合成1个事件 & 发送
+        //聚合的逻辑根据需求撰写，但本质都是前2个数据聚合，然后与后1个数据继续进行聚合，依次类推
+        Observable.just(1,2,3,4,5,6)
+                .reduce(new BiFunction<Integer, Integer, Integer>() {
+                    @Override
+                    public Integer apply(Integer s1, Integer s2) throws Exception {
+                        Log.i(TAG, "本次计算的数据是： "+s1 +" 乘 "+ s2);
+                        return s1*s2;
+                        // 本次聚合的逻辑是：全部数据相乘起来
+                        // 原理：第1次取前2个数据相乘，之后每次获取到的数据 = 返回的数据x原始下1个数据每
+                    }
+                }).subscribe(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) throws Exception {
+                Log.i(TAG, "最终计算结果："+integer);
+            }
+        });
+    }
+
+    private void testCollect() {
+        //将被观察者Observable发送的数据事件收集到一个数据结构里
+        Observable.just(1,2,3,4,5,6)
+                .collect(
+                        // 1. 创建数据结构（容器），用于收集被观察者发送的数据
+                        new Callable<ArrayList<Integer>>() {
+                            @Override
+                            public ArrayList<Integer> call() throws Exception {
+                                return new ArrayList<>();
+                            }
+                            // 2. 对发送的数据进行收集
+                        }, new BiConsumer<ArrayList<Integer>, Integer>() {
+                            @Override
+                            public void accept(ArrayList<Integer> list, Integer integer) throws Exception {
+                                // 参数说明：list = 容器，integer = 后者数据
+                                list.add(integer);// 对发送的数据进行收集
+                            }
+                        })
+                .subscribe(new Consumer<ArrayList<Integer>>() {
+                    @Override
+                    public void accept(ArrayList<Integer> list) throws Exception {
+                        Log.i(TAG, "本次发送的数据是： "+list);
+                    }
+                });
+    }
+
+    private void testStartWith() {
+        //在一个被观察者发送事件前，追加发送一些数据 / 一个新的被观察者
+        //追加数据顺序 = 后调用先追加
+        Observable.just(4, 5, 6)
+                .startWith(0) // 追加单个数据 = startWith()
+                .startWithArray(1, 2, 3) // 追加多个数据 = startWithArray()
+                .subscribe(new LogObserver<Integer>("testStartWith"));
+
+        Log.i(TAG, "-------------------------------------");
+
+        Observable.just(4,5,6)
+                .startWith(Observable.just(1,2,3))
+                .subscribe(new LogObserver<Integer>("testStartWith2"));
+    }
+
+    private void testCount() {
+        //统计被观察者发送事件的数量
+        Observable.just(1,2,3,4,5,6,7)
+                .count()
+                .subscribe(new Consumer<Long>() {
+                    @Override
+                    public void accept(Long count) throws Exception {
+                        Log.i(TAG, "发送的事件数量 =  "+count);
                     }
                 });
     }
