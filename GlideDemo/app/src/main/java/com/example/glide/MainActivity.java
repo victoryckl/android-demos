6505e7d17c34c46d923da8d4a59ae3fa0e501683
victoryckl <137456570@qq.com>
2022-06-19 15:50:06 +0800

    GlideDemo 	new file:   GlideDemo/.gitignore 	new file:   GlideDemo/app/.gitignore 	new file:   GlideDemo/app/build.gradle 	new file:   GlideDemo/app/proguard-rules.pro 	new file:   GlideDemo/app/src/androidTest/java/com/example/glide/ExampleInstrumentedTest.java 	new file:   GlideDemo/app/src/main/AndroidManifest.xml 	new file:   GlideDemo/app/src/main/java/com/example/glide/MainActivity.java 	new file:   GlideDemo/app/src/main/res/drawable-v24/ic_launcher_foreground.xml 	new file:   GlideDemo/app/src/main/res/drawable/ic_launcher_background.xml 	new file:   GlideDemo/app/src/main/res/layout/activity_main.xml 	new file:   GlideDemo/app/src/main/res/mipmap-anydpi-v26/ic_launcher.xml 	new file:   GlideDemo/app/src/main/res/mipmap-anydpi-v26/ic_launcher_round.xml 	new file:   GlideDemo/app/src/main/res/mipmap-hdpi/ic_launcher.webp 	new file:   GlideDemo/app/src/main/res/mipmap-hdpi/ic_launcher_round.webp 	new file:   GlideDemo/app/src/main/res/mipmap-mdpi/ic_launcher.webp 	new file:   GlideDemo/app/src/main/res/mipmap-mdpi/ic_launcher_round.webp 	new file:   GlideDemo/app/src/main/res/mipmap-xhdpi/ic_launcher.webp 	new file:   GlideDemo/app/src/main/res/mipmap-xhdpi/ic_launcher_round.webp 	new file:   GlideDemo/app/src/main/res/mipmap-xxhdpi/ic_launcher.webp 	new file:   GlideDemo/app/src/main/res/mipmap-xxhdpi/ic_launcher_round.webp 	new file:   GlideDemo/app/src/main/res/mipmap-xxxhdpi/ic_launcher.webp 	new file:   GlideDemo/app/src/main/res/mipmap-xxxhdpi/ic_launcher_round.webp 	new file:   GlideDemo/app/src/main/res/values-night/themes.xml 	new file:   GlideDemo/app/src/main/res/values/colors.xml 	new file:   GlideDemo/app/src/main/res/values/strings.xml 	new file:   GlideDemo/app/src/main/res/values/themes.xml 	new file:   GlideDemo/app/src/test/java/com/example/glide/ExampleUnitTest.java 	new file:   GlideDemo/build.gradle 	new file:   GlideDemo/gradle.properties 	new file:   GlideDemo/gradle/wrapper/gradle-wrapper.jar 	new file:   GlideDemo/gradle/wrapper/gradle-wrapper.properties 	new file:   GlideDemo/gradlew 	new file:   GlideDemo/gradlew.bat 	new file:   GlideDemo/settings.gradle

---
+++ GlideDemo/app/src/main/java/com/example/glide/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.glide;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.FutureTarget;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.target.Target;
+import com.bumptech.glide.request.transition.Transition;
+
+import java.io.File;
+import java.util.SimpleTimeZone;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = findViewById(R.id.iv_image);
+
+        if (checkForPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUEST_CODE)) {
+            startGlide();
+        }
+    }
+
+    private static final int REQUEST_CODE = 100;
+
+    private boolean checkForPermission(String permission, int requestCode) {
+        // Check if permission is already granted.
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission has not been granted. Check if a rationale AlertDialog should be shown.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+
+                // Show rationale AlertDialog explaining why this permission is needed.
+                new AlertDialog.Builder(this)
+                        .setTitle("请求权限")
+                        .setMessage("拥有写入权限才能保存图片")
+                        .setPositiveButton(android.R.string.ok,
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        // Request permission again.
+                                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+                                    }
+                                })
+                        .create()
+                        .show();
+            }
+
+            // Do not show rationale AlertDialog. Just request permission again.
+            else {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (REQUEST_CODE == requestCode) {
+            boolean allowed = true;
+            for (int grantResult : grantResults) {
+                if (grantResult != PackageManager.PERMISSION_GRANTED) {
+                    allowed = false;
+                    break;
+                }
+            }
+            if (allowed) startGlide();
+        }
+    }
+
+    private static final String IMAGE_URL = "http://pic2.zhimg.com/v2-a5b909e499c3b0437b44785bd7512d39_r.jpg";
+
+    private void startGlide() {
+        Glide.with(this)
+                .load(Uri.parse(IMAGE_URL))
+                .into(mImageView);
+        Glide.with(this)
+                .asBitmap()
+                .load(Uri.parse(IMAGE_URL))
+                .downloadOnly(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL);
+//                .into(new SimpleTarget<byte[]>() {
+//
+//                    @Override
+//                    public void onResourceReady(@NonNull byte[] resource, @Nullable Transition<? super byte[]> transition) {
+//
+//                    }
+//                });
+    }
+}
